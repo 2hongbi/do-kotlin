61a0b0c8a4c4f0051055af9fb7b4c586ec92f971
hongbi-lee <foxgirin950@gmail.com>
2022-03-28 23:11:15 +0900

    done with page 73

--- src/main/kotlin/chap02/section2/NullTest.kt
+++ src/main/kotlin/chap02/section2/NullTest.kt
@@ -1,7 +1,10 @@
 package chap02.section2
 
 fun main() {
-    var str1: String = "Hello Kotlin"
-    // str1 = null // 코틀린은 변수에 null 허용 안함
-    println("str1: $str1")
+    var str1: String? = "Hello Kotlin"
+    str1 = null // 코틀린은 변수에 null 허용 안함. 하지만, 자료형 뒤에 물음표(?) 기호를 명시하면 null 값 할당 가능
+    println("str1: $str1 length: ${str1?.length}")  // null 허용시, length가 실행될 수 없음 > str1을 세이프콜로 안전하게 호출
+
+    var len = if(str1 != null) str1.length else -1
+    println("str1: $str1 length: ${len}")
 }
