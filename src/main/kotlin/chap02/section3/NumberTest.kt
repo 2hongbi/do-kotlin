6d423bd378083928d0aadc412f387c533a2b2c99
hongbi-lee <foxgirin950@gmail.com>
2022-04-05 23:54:02 +0900

    done with page 80

--- src/main/kotlin/chap02/section3/NumberTest.kt
+++ src/main/kotlin/chap02/section3/NumberTest.kt
@@ -1,2 +1,15 @@
 package chap02.section3
 
+fun main() {
+    var test : Number = 12.2
+    println("$test")
+
+    test = 12 // int형으로 스마트 캐스트
+    println("$test")
+
+    test = 120L // Long으로 스마트 캐스트
+    println("$test")
+
+    test += 12.0f // Float형으로 스마트 캐스트
+    println("$test")
+}
