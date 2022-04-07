57dee1b1579ac407d9b72389eeed747c20a4138f
hongbi-lee <foxgirin950@gmail.com>
2022-04-07 19:14:18 +0900

    done with page 90

---
+++ src/main/kotlin/chap02/section4/BitsShift.kt
@@ -0,0 +1,12 @@
+package chap02.section4
+
+fun main() {
+    var x = 4
+    var y = 0b0000_1010 // 10진수 5
+    var z = 0x0F // 10 진수 15
+
+    println("x sh1 2 -> ${x shl 2}")
+    println("x.inv() -> ${x.inv()}")
+
+
+}
